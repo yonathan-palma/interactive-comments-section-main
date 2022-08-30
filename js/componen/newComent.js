450e1b32d000fffa2792dfbcba67aa46da677608
yonathan <yonathanpalmarey@gmail.com>
2022-08-30 01:03:43 -0400

    Choose a user

---
+++ js/componen/newComent.js
@@ -0,0 +1,36 @@
+import mostarData from "./comments.js";
+
+function newComment(e){
+
+    e.preventDefault();
+
+    const textComment = document.getElementById("textComment");
+    let comentarios = JSON.parse(localStorage.commeentAdd).comments;
+    let user = JSON.parse(localStorage.commeentAdd).currentUser;
+
+    let newComment = {
+        id: comentarios.length + 1,
+        content: textComment.value,
+        createdAt: "1 month ago",
+        score: 0,
+        user: {
+          image: { 
+            png: `../images/avatars/image-${user.username}.png`, 
+            webp: `../images/avatars/image-${user.username}.webp`
+        },
+          username: user.username
+        },
+        replies: []
+    };
+    
+    comentarios.push(newComment);
+    let newData = {
+        currentUser: {...user},
+        comments: [...comentarios]
+    }
+
+    mostarData(JSON.stringify(newData));
+    textComment.value = "";  
+}
+
+export default newComment;
--- js/index.js
+++ js/index.js
@@ -1,53 +1,18 @@
 import getData from "./dataInit/dataInit.js";
 import mostarData from "./componen/comments.js";
+import newComment from "./componen/newComent.js";
 
 const data = './js/data.json';
 const response = await getData(data);
 
 const btnComment = document.getElementById("comment");
-const textComment = document.getElementById("textComment");
-
 btnComment.addEventListener("submit", newComment);
 
-function newComment(e){
+const btnList = document.querySelectorAll(".offcanvasli button");
+let user = JSON.parse(localStorage.commeentAdd).currentUser;
 
-    e.preventDefault();
-
-    let comentarios = JSON.parse(localStorage.commeentAdd).comments;
-    let user = JSON.parse(localStorage.commeentAdd).currentUser;
-
-    let newComment = {
-        id: comentarios.length + 1,
-        content: textComment.value,
-        createdAt: "1 month ago",
-        score: 0,
-        user: {
-          image: { 
-            png: `./images/avatars/image-${user.username}.png`, 
-            webp: `./images/avatars/image-${user.username}.webp`
-        },
-          username: user.username
-        },
-        replies: []
-    };
-    
-    comentarios.push(newComment);
-    let newData = {
-        currentUser: {...user},
-        comments: [...comentarios]
-    }
-
-    mostarData(JSON.stringify(newData));
-    textComment.value = "";
-    
-    
-}
+btnList.forEach(element => {
+    element.dataset.currentUser == user.username ? element.classList.add("current") : '';
+})
 
 mostarData(response);
-
-
-// console.log(template)
-
-
-
-// console.log(initialState());
